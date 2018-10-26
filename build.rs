241e3da832dfe9cde434d326299b73231d0856db
Marc-Antoine Jacob <marc-antoinejacob@outlook.com>
2018-10-26 12:28:29 -0400

    handle life just don't panic

--- build.rs
+++ build.rs
@@ -2,9 +2,9 @@ extern crate vergen;
 
 fn main() {
     if let Err(err) = vergen::vergen(vergen::OutputFns::all()) {
-        panic!(
+        dontpanic!(
             "Vergen crate failed to generate version information! {:?}",
-            err
+            err,
         );
     }
 }
