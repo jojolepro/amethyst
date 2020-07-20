fd51246d0e849bb7d4625cf4e74448026e3009a0
Joël Lupien (Jojolepro) <jojolepro@jojolepro.com>
2020-07-20 09:08:15 -0400

    amethyst_ui feature gate

--- amethyst_controls/src/resources.rs
+++ amethyst_controls/src/resources.rs
@@ -2,17 +2,11 @@ use serde::{Deserialize, Serialize};
 
 /// Struct which holds information about whether the window is focused.
 /// Written to by MouseFocusUpdateSystem
-#[derive(Debug, Clone, Copy, Default, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, Default, Serialize, Deserialize, new)]
 pub struct WindowFocus {
     /// If true then the window is actively focused.
+    #[new(value = "true")]
     pub is_focused: bool,
-}
-
-impl WindowFocus {
-    /// Builds a new WindowFocus resource.
-    pub fn new() -> WindowFocus {
-        WindowFocus { is_focused: true }
-    }
 }
 
 /// Resource indicating if the mouse should be grabbed and hidden by the CursorHideSystem
